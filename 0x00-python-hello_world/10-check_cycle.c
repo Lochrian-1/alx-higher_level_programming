f9c64cf7b9bddb5f3d89687fa59fec0b03eb3895
Lochrian-1 <workbusmax@gmail.com>
2022-08-22 16:51:17 +0200

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * list - linked list
+ *
+ * Return: 0 if cycle, 1 if no cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+	
+	slow = list;
+	fast = list;
+
+	while (slow && fast)
+	{
+		if (fast->next == NULL)
+		{
+			return (0);
+		}
+		
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
