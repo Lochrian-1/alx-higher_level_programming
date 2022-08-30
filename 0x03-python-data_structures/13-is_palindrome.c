67d9aac8c811e5891efceb120aae565e187b55cc
Lochrian-1 <workbusmax@gmail.com>
2022-08-30 05:20:42 +0200

    updates

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,73 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: head of linked list
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
  *
- * Return: void
+ * Return: pointer to the first node in the new list
+ */
+
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the linked list
+ *
+ * Return: 1 if it is, 0 if not
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *rev_list, *node, *next, *previous;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	rev_list = malloc(sizeof(listint_t));
-
-	node = *head;
-	previous = NULL;
-	while (node != NULL)
+	while (1)
 	{
-		next = node->next;
-		node->next = previous;
-		previous = node;
-		node = next;
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	node = previous;
-	rev_list = node;
 
+	reverse_listint(&dup);
 
-	if (*head == rev_list)
+	while (dup && temp)
 	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
 }
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-
-    if len(matrix[0]) != 0:
-        rows = len(matrix)
-        columns = len(matrix[0])
-
-        for i in range(rows):
-            for j in range(columns):
-                if j == columns - 1:
-                    print("{:d}".format(matrix[i][j]))
-                else:
-                    print("{:d}".format(matrix[i][j]), end=" ")
-     else:
-         print()
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
