89b1287e62633f1d4308f98936675de29e800fbe
Lochrian-1 <workbusmax@gmail.com>
2022-08-31 00:31:45 +0200

    task updates

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
-    
-    addition = add(a, b)
-    subtract = sub(a, b)
-    multiply = mul(a, b)
-    divide = div(a, b)
 
-    print("{:d} + {:d} = {:d}".format(a, b, addition))
-    print("{:d} - {:d} = {:d}".format(a, b, subtract))
-    print("{:d} * {:d} = {:d}".format(a, b, multiply))
-    print("{:d} / {:d} = {:d}".format(a, b, divide))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import sys
 
-    from sys import argv
-
-    result = 0
-
-    if len(argv) == 1:
-        print("0")
-    else:
-        my_tuple = (1, len(argv))
-
-    for i in range(my_tuple[0], my_tuple[1]):
-        result += int(argv[i])
-
-    print("{:d}".format(result))
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
