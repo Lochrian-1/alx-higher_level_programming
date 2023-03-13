6fdfc8d6d5c6c12f7cb406818119ef76637df9e7
Lochrian-1 <workbusmax@gmail.com>
2023-03-13 23:58:38 +0200

    task1*

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,4 @@
 const myVar1 = 'C is fun\n';
 const myVar2 = 'Python is cool\n';
 const myVar3 = 'JavaScript is amazing';
-console.log(myVar1);
-console.log(myVar2);
-console.log(myVar3);
+console.log(myVar1, myVar2, myVar3);
